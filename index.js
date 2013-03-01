24f8a7629350c79f61294a8d429654e672d5cc1f
Anders Fisher <atleastimtrying@gmail.com>
2013-03-01 08:34:52 +0000

    herokued

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var connect = require("connect");
 var app = connect().use(connect.static('public')).listen(process.env.PORT || 8080);
-var io = require('socket.io').listen(app);
-// io.configure(function () { 
-//   io.set("transports", ["xhr-polling"]); 
-//   io.set("polling duration", 10); 
-// });
+var io = require('socket.io');
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+io.listen(app);
 
 var Phrases = function(){
   var phrases = [
